0fe77482d6511c6f80e7f1ff39b2431a28a64e3a
trantuanminh-git <trantuanminh9802@gmail.com>
2023-04-27 13:14:43 +0700

    ora2_first_commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TTSV = {
+let TTSV = {
     imageURL: "./img/TTM.png",
     MSSV: "20204842",
     "Họ và tên": "Trần Tuấn Minh",
@@ -14,8 +14,84 @@ const TTSV = {
 };
 
 function edit() {
-    // alert(TTSV["imageURL"]);
-    document.getElementsByClassName("ora2")[0].removeAttribute("disabled");
-    console.log(document.getElementsByClassName("ora2"))
+    // show the button
+    [...document.getElementsByClassName("info2_input")].forEach((element) => {
+        element.removeAttribute("disabled");
+    });
+    document.getElementsByClassName("btnAppear")[0].removeAttribute("style");
+
+    // console.log(currentValue)
+    // console.log(document.getElementsByClassName("btnAppear"));
+    // console.log(document.getElementsByClassName("info2_input"));
     // alert(TTSV["heml"]);
 }
+
+function handleOK() {
+    console.log("handle ok");
+    [...document.getElementsByClassName("info2_input")].forEach((element) => {
+        element.setAttribute("disabled", "");
+    });
+    document
+        .getElementsByClassName("btnAppear")[0]
+        .setAttribute("style", "display: none");
+
+    // const val = document.querySelector("input").value;
+    const label = document.getElementsByClassName("info2_label");
+    const input = document.getElementsByClassName("info2_input");
+    for (let i = 0; i < label.length; i++) {
+        // console.log(label[i])
+        // TTSV[label[i].textContent] = input[i].value;
+        // console.log('check')
+        // console.log(label[i].textContent)
+        let x = label[i].textContent;
+        TTSV[x.slice(0, x.indexOf(" "))] = input[i].value;
+        // console.log(TTSV[x.slice(0,x.indexOf(" "))])
+    }
+    console.log("TTSV OK button");
+    console.log(TTSV);
+    // console.log(input);
+    // console.log(label);
+}
+
+function handleCancel() {
+    console.log("handle cancel");
+    [...document.getElementsByClassName("info2_input")].forEach((element) => {
+        element.setAttribute("disabled", "");
+    });
+    document
+        .getElementsByClassName("btnAppear")[0]
+        .setAttribute("style", "display: none");
+    const label = document.getElementsByClassName("info2_label");
+    const input = document.getElementsByClassName("info2_input");
+    for (let i = 0; i < label.length; i++) {
+        let x = label[i].textContent;
+        input[i].value = TTSV[x.slice(0, x.indexOf(" "))];
+        // console.log(TTSV)
+    }
+    console.log("TTSV cancel button");
+    console.log(TTSV);
+}
+
+function handleReset() {
+    console.log("handle reset");
+    console.log("TTSV reset button");
+    console.log(TTSV);
+}
+
+function loadImage(event) {
+    // const imgBox = document.getElementById
+    const imgBox = document.getElementsByClassName("avatarImage")[0];
+    // const currentImg = document.querySelector(".avatarImage img");
+    // console.log(currentImg)
+    // currentImg.setAttribute("src", '')
+    
+    
+    // img.src = URL.createObjectURL(event.target.file[0]);
+    imgBox.style.backgroundImage =
+        "url(" + URL.createObjectURL(event.target.files[0]) + ")";
+        console.log(event.target.files[0])
+}
+
+const formInfo2 = document
+    .getElementById("info2_infomation")
+    .addEventListener("submit", (e) => e.preventDefault());
